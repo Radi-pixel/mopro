9b31c4f138bfb3f8ce51e72a00eb161ed0dfa362
Radi-pixel <radihardansyah@student.telkomuniversity.ac.id>
2023-07-26 17:36:33 +0700

    3.1 menambahkan tombol next

--- app/src/main/java/org/d3if0052/helloworld/MainActivity.kt
+++ app/src/main/java/org/d3if0052/helloworld/MainActivity.kt
@@ -2,10 +2,16 @@ package org.d3if0052.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nextButton: Button = findViewById(R.id.nextButton)
+        nextButton.setOnClickListener { showNext() }
+    }
+    private fun showNext() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
