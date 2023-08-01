4a593d86e04c8d123f91b1f38c42245cee514f6f
Radi-pixel <radihardansyah@student.telkomuniversity.ac.id>
2023-08-01 20:11:22 +0700

    3.2 Membuat notification channel

--- app/src/main/java/org/d3if0052/helloworld/MainActivity.kt
+++ app/src/main/java/org/d3if0052/helloworld/MainActivity.kt
@@ -1,5 +1,9 @@
 package org.d3if0052.helloworld
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -8,12 +12,26 @@ import org.d3if0052.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val CHANNEL_ID = "updater"
+    }
+
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_desc)
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager?
+            manager?.createNotificationChannel(channel)
+        }
 
         with(binding.recyclerView) {
             addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
