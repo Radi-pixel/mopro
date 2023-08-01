2f102164bb6c2cf8438e1214e5f1811469747e70
Radi-pixel <radihardansyah@student.telkomuniversity.ac.id>
2023-08-02 00:01:08 +0700

    3.3 Meminta izin notifikasi

--- app/src/main/java/org/d3if0052/helloworld/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if0052/helloworld/ui/main/MainFragment.kt
@@ -1,4 +1,26 @@
 package org.d3if0052.helloworld.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import org.d3if0052.helloworld.MainActivity
+
 class MainFragment {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestNotificationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                MainActivity.PERMISSION_REQUEST_CODE
+            )
+        }
+    }
 }
